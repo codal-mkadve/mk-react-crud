0002007f94ddd23d70f3af8742e4e350bde55552
codal-mkadve <mkadve@codal.com>
2024-02-19 22:23:23 +0530

    Fixed form validations

--- src/Components/Users/Add.jsx
+++ src/Components/Users/Add.jsx
@@ -1,4 +1,4 @@
-import { useState , useCallback } from "react";
+import { useState, useCallback } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { Button, ButtonGroup, Card, Col, Form, Row } from "react-bootstrap";
 import { getUserById, userFormValidation } from "../../Services/user-service";
@@ -16,7 +16,6 @@ const defaultUser = {
   status: "active",
 };
 
-
 const genders = {
   M: "Male",
   F: "Female",
@@ -28,12 +27,12 @@ export const statuses = {
   inactive: "Inactive",
 };
 
-const UsersAdd  = ({ id = "" }) => {
+const UsersAdd = ({ id = "" }) => {
   const [isLoading, setLoading] = useState(false);
   const navigate = useNavigate();
   const isEdit = !!id;
 
-  const handleFormSubmit = (values) =>{
+  const handleFormSubmit = (values) => {
     // setValue("firstName", generateRandomUser().firstName);
     // setValue("lastName", generateRandomUser().lastName);
     // setValue("email", generateRandomUser().email);
@@ -41,20 +40,24 @@ const UsersAdd  = ({ id = "" }) => {
     // setValue("note", generateRandomUser().note);
     // setValue("gender", generateRandomUser().gender);
     // setValue("status", generateRandomUser().status);
-    console.log('values',values)
-  }
-  
-  const { register,handleSubmit, watch, formState: { errors }, setValue, getValues } = useForm({
+    console.log("values", values);
+  };
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    setValue,
+    getValues,
+  } = useForm({
     defaultValues: defaultUser,
     resolver: yupResolver(userFormValidation()),
-    
   });
 
-  console.log("aaa", watch())
+  console.log("aaa", watch(),errors);
 
-  const handleRandomData = () => {
-   
-  };
+  const handleRandomData = () => {};
 
   const renderSubmit = () => {
     if (isLoading) {
@@ -90,27 +93,41 @@ const UsersAdd  = ({ id = "" }) => {
             <Col md="6">
               <Form.Group>
                 <Form.Label htmlFor="firstName">First Name </Form.Label>
-                <Form.Control id="firstName" name="firstName"
-                invalid={!!errors?.firstName}
-                {...register("firstName")}/>
-                <Form.Control.Feedback>{errors?.firstName}</Form.Control.Feedback>
+                <Form.Control
+                  id="firstName"
+                  name="firstName"
+                  isInvalid={!!errors?.firstName}
+                  {...register("firstName")}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors?.firstName?.message}
+                </Form.Control.Feedback>
               </Form.Group>
             </Col>
             <Col md="6">
               <Form.Group>
-                <Form.Label for="lastName">Last Name </Form.Label>
-                <Form.Control id="lastName" name="lastName" invalid={!!errors?.lastName}
-                {...register("lastName")}/>
-                <Form.Control.Feedback>{errors?.lastName}</Form.Control.Feedback>
+                <Form.Label htmlFor="lastName">Last Name </Form.Label>
+                <Form.Control
+                  id="lastName"
+                  name="lastName"
+                  isInvalid={!!errors?.lastName}
+                  {...register("lastName")}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors?.lastName?.message}
+                </Form.Control.Feedback>
               </Form.Group>
             </Col>
           </Row>
           <Form.Group>
-            <Form.Label for="email">Email Address </Form.Label>
-            <Form.Control id="email" name="email"
-            invalid={!!errors?.email}
-            {...register("email")}/>
-            <Form.Control.Feedback>{errors?.email}</Form.Control.Feedback>
+            <Form.Label htmlFor="email">Email Address </Form.Label>
+            <Form.Control
+              id="email"
+              name="email"
+              isInvalid={!!errors?.email}
+              {...register("email")}
+            />
+            <Form.Control.Feedback type="invalid">{errors?.email?.message}</Form.Control.Feedback>
           </Form.Group>
           <Form.Group>
             <Form.Label>Gender </Form.Label>
@@ -122,7 +139,6 @@ const UsersAdd  = ({ id = "" }) => {
                     color="secondary"
                     key={value}
                     onClick={() => setValue("gender", value)}
-                    outline
                     type="button"
                   >
                     {genders[value]}
@@ -132,41 +148,53 @@ const UsersAdd  = ({ id = "" }) => {
             </div>
           </Form.Group>
           <Form.Group>
-            <Form.Label for="address">Address</Form.Label>
-            <Form.Control id="address" name="address" type="textarea" 
-            invalid={!!errors?.address}
-            {...register("address")}
-           />
-            <Form.Control.Feedback>{errors?.address}</Form.Control.Feedback>
+            <Form.Label htmlFor="address">Address</Form.Label>
+            <Form.Control
+              id="address"
+              name="address"
+              type="textarea"
+              isInvalid={!!errors?.address}
+              {...register("address")}
+            />
+            <Form.Control.Feedback>{errors?.address?.message}</Form.Control.Feedback>
           </Form.Group>
           <Form.Group>
-            <Form.Label for="note">Note</Form.Label>
-            <Form.Control id="note" name="note" type="textarea" invalid={!!errors?.note}
-            {...register("note")}/>
-            <Form.Control.Feedback>{errors?.note}</Form.Control.Feedback>
+            <Form.Label htmlFor="note">Note</Form.Label>
+            <Form.Control
+              id="note"
+              name="note"
+              type="textarea"
+              isInvalid={!!errors?.note}
+              {...register("note")}
+            />
+            <Form.Control.Feedback type="invalid">{errors?.note?.message}</Form.Control.Feedback>
           </Form.Group>
           <Form.Group className="pb-4">
             <Form.Label>Status </Form.Label>
             <div>
               <ButtonGroup>
-              {Object.keys(statuses).map((key) => (
-                <Button
-                  key={key}
-                  color="secondary"
-                  onClick={() => setValue("status", key)}
-                  outline
-                  type="button"
-                  active={getValues("status") === key}
-                >
-                  {statuses[key]}
-                </Button>
-              ))}
+                {Object.keys(statuses).map((key) => (
+                  <Button
+                    key={key}
+                    color="secondary"
+                    onClick={() => setValue("status", key)}
+                    type="button"
+                    active={getValues("status") === key}
+                  >
+                    {statuses[key]}
+                  </Button>
+                ))}
               </ButtonGroup>
             </div>
           </Form.Group>
           <Form.Group className="d-flex justify-content-between">
-            <Button color="primary" type="submit" disabled={isLoading} style={{ minWidth: 200 }}>
-            {renderSubmit()}
+            <Button
+              color="primary"
+              type="submit"
+              disabled={isLoading}
+              style={{ minWidth: 200 }}
+            >
+              {renderSubmit()}
             </Button>
             <Button
               color="secondary"
